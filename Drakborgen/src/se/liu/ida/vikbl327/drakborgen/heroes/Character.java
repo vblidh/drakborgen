bc0647fa13a0fcbe3b6ba4f4ef43fcfe01e663e3
Viktor <vikbl327@student.liu.se>
2019-05-04 11:00:10 +0200

    Adding stash

---
+++ Drakborgen/src/se/liu/ida/vikbl327/drakborgen/heroes/Character.java
@@ -0,0 +1,50 @@
+package se.liu.ida.vikbl327.drakborgen.heroes;
+
+import javax.swing.*;
+
+/**
+ * Interface for all the different heroes that can be chosen to play as at the start of a game. In future implementations heroes
+ * will have specific abilities and therefore specific methods, hence the interface.
+ */
+
+public interface Character
+{
+    public String getName();
+
+    public Object[] getAttackOptions();
+
+    public int getDoubleDamageAttackIndex();
+
+    public int getHealthPoints();
+
+    public int getStrengthFactor();
+
+    public int getAgilityFactor();
+
+    public int getArmorFactor();
+
+    public int getLuckFactor();
+
+    public int getCurrentHealth();
+
+    public void setCurrentHealth(final int currentHealth);
+
+    public int getxPos();
+
+    public int getyPos();
+
+    public ImageIcon getHeroIcon();
+
+    public double getScaleX();
+
+    public double getScaleY();
+
+    public int getCalculatedxPos();
+
+    public int getCalculatedyPos();
+
+    public void setxPos(final int xPos);
+
+    public void setyPos(final int yPos);
+
+}
---
+++ Drakborgen/src/se/liu/ida/vikbl327/drakborgen/heroes/GenericHero.java
@@ -0,0 +1,13 @@
+package se.liu.ida.vikbl327.drakborgen.heroes;
+
+public abstract class GenericHero implements Character
+{
+
+    private int xPos;
+    private int yPos;
+
+    public GenericHero() {
+        this.xPos = 0;
+        this.yPos = 0;
+    }
+}
